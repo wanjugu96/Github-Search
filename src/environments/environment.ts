37b39f14bcb0f1ebdeb8003485d56d93513ba11e
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-08-30 10:25:47 +0300

    securing api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:"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"
 };
 
 /*
