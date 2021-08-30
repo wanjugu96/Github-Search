6583aeb6fbb6ae6397a54ca612d1bb484c3f3912
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-08-30 10:07:10 +0300

    adding http request to get profile data

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Users } from '../users';
 
 @Component({
   selector: 'app-profiles',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profiles.component.css']
 })
 export class ProfilesComponent implements OnInit {
-
-  constructor() { }
+user!:Users;
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    interface ApiResponse{
+      login:string;
+       name:string;
+        company:string; 
+        followers:number;
+        following:number;
+         public_repos:number ;
+    }
+    
+    this.http.get<ApiResponse>("https://api.github.com/users/wanjugu96").subscribe(data=>{
+      //successfull Api request
+      this.user=new Users(data.login,data.name,data.company,data.followers,data.following,data.public_repos)
+  },err=>
+  this.user=new Users("Wanjugu Mungau","Wanjugu M","Getbee UAE",1,9,33)
+ // console.log("an Error occured.")
+    )
   }
-
 }
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,3 @@
 export class Users {
+    constructor(public login:string,public name:string,public company:string,public followers:number,public following:number,public public_repos:number ){}
 }
